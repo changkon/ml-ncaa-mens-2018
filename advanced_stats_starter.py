705e856163df48ea8582b82a66a40907b40ff72e
John Law <johnlaw1234@hotmail.com>
2018-03-02 20:12:35 +1300

    Update to calculate pos after sum all stats first

--- advanced_stats_starter.py
+++ advanced_stats_starter.py
@@ -7,26 +7,26 @@ pd.set_option('display.width', 1000)
 submission = pd.read_csv("input/SampleSubmissionStage1.csv")
 playoff_results = pd.read_csv("input/NCAATourneyDetailedResults.csv")
 seasons_results = pd.read_csv("input/RegularSeasonDetailedResults.csv")
+seasons_results.drop(['NumOT', 'DayNum'], axis=1, inplace=True)
 
 
 # https://www.basketball-reference.com/about/glossary.html
 def get_pos(row):
-    w_half = row.WFGA + 0.4*row.WFTA - 1.07*(row.WOR / (row.WOR + row.LOR))*(row.WFGA - row.WFGM) + row.WTO
-    l_half = row.LFGA + 0.4*row.LFTA - 1.07*(row.LOR / (row.LOR + row.WOR))*(row.LFGA - row.LFGM) + row.LTO
+    w_half = row.WFGA + 0.4*row.WFTA - 1.07*(row.WOR / (row.WOR + row.LDR))*(row.WFGA - row.WFGM) + row.WTO
+    l_half = row.LFGA + 0.4*row.LFTA - 1.07*(row.LOR / (row.LOR + row.WDR))*(row.LFGA - row.LFGM) + row.LTO
     return 0.5*(w_half+l_half)
 
-seasons_results['Pos'] = seasons_results.apply(lambda row: get_pos(row), axis=1)
+win_stats = ['WTeamID', 'WScore', 'WFGM', 'WFGA', 'WFGM3', 'WFGA3', 'WFTM', 'WFTA', 'WOR', 'WDR', 'WAst', 'WTO', 'WStl', 'WBlk', 'WPF']
+lose_stats = ['LTeamID', 'LScore', 'LFGM', 'LFGA', 'LFGM3', 'LFGA3', 'LFTM', 'LFTA', 'LOR', 'LDR', 'LAst', 'LTO', 'LStl', 'LBlk', 'LPF']
 
-seasons_results_cpy = seasons_results.copy()
-seasons_results_cpy[['WTeamID', 'WScore', 'LTeamID', 'LScore']] = seasons_results_cpy[['LTeamID', 'LScore', 'WTeamID', 'WScore']]
+seasons_results_copy = seasons_results.copy()
+seasons_results_copy[win_stats + lose_stats] = seasons_results_copy[lose_stats + win_stats]
 
-seasons_results = pd.concat([seasons_results, seasons_results_cpy], ignore_index=True)
+seasons_results = pd.concat([seasons_results, seasons_results_copy], ignore_index=True)
 
-adv_stat_team_season = seasons_results.groupby(['Season', 'WTeamID'])['Pos', 'WScore', 'LScore'].sum()
-# pos_team_season = season_games.groupby(['Season', 'WTeamID']).size().reset_index(name='count')
+adv_stat_team_season = seasons_results.groupby(['Season', 'WTeamID']).sum()
+adv_stat_team_season['Pos'] = adv_stat_team_season.apply(lambda row: get_pos(row), axis=1)
 adv_stat_team_season['ORating'] = adv_stat_team_season.apply(lambda row: 100*row.WScore/row.Pos, axis=1)
 adv_stat_team_season['DRating'] = adv_stat_team_season.apply(lambda row: 100*row.LScore/row.Pos, axis=1)
-adv_stat_team_season = adv_stat_team_season.reset_index()
-
-adv_stat_team_season.drop('Pos', axis=1, inplace=True)
 print(adv_stat_team_season.head())
+
