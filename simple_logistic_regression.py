6ad01a4d96e7b642d502dedbbf232a8f7ce2b437
John Law <johnlaw1234@hotmail.com>
2018-03-10 17:38:46 +1300

    well steady state

--- simple_logistic_regression.py
+++ simple_logistic_regression.py
@@ -14,6 +14,8 @@ compact_playoff_results = pd.read_csv("input/NCAATourneyCompactResults.csv")
 playoff_seeds = pd.read_csv("input/NCAATourneySeeds.csv")
 seasons_results = pd.read_csv("input/RegularSeasonDetailedResults.csv")
 adv_stat_team_season = get_adv_season_stats()
+adj_adv_stat_team_season = pd.read_csv("p_rating_with_team_id.csv")
+adj_adv_stat_team_season = adj_adv_stat_team_season.drop(['AdjTempo', 'AdjORating', 'AdjDRating'], axis=1)
 
 def seed_to_int(seed):
     #Get just the digits from the seeding. Return as int
@@ -21,12 +23,21 @@ def seed_to_int(seed):
     return s_int
 
 # lets use data from 2003-2013 to train the modal
-train = compact_playoff_results.loc[(compact_playoff_results['Season'] >= 2010) & (compact_playoff_results['Season'] <= 2013)]
+train = compact_playoff_results.loc[(compact_playoff_results['Season'] >= 2003) & (compact_playoff_results['Season'] <= 2013)]
 train = train.drop(['DayNum', 'WScore', 'LScore', 'WLoc', 'NumOT'], axis=1)
 
 train_stats = ['Season', 'TeamID', 'ORating', 'DRating']
-win_adv_stats = adv_stat_team_season[train_stats].rename(columns={'TeamID': 'WTeamID', 'ORating': 'WORating', 'DRating': 'WDRating'})
-lose_adv_stats = adv_stat_team_season[train_stats].rename(columns={'TeamID': 'LTeamID', 'ORating': 'LORating', 'DRating': 'LDRating'})
+win_adv_stats = adv_stat_team_season[train_stats].rename(columns={
+    'TeamID': 'WTeamID',
+    'ORating': 'WORating',
+    'DRating': 'WDRating'
+})
+
+lose_adv_stats = adv_stat_team_season[train_stats].rename(columns={
+    'TeamID': 'LTeamID',
+    'ORating': 'LORating',
+    'DRating': 'LDRating'
+})
 train = train.merge(win_adv_stats, on=['Season', 'WTeamID'])
 train = train.merge(lose_adv_stats, on=['Season', 'LTeamID'])
 
@@ -40,21 +51,42 @@ lossseeds = playoff_seeds.rename(columns={'TeamID': 'LTeamID', 'seed_int': 'LSee
 train = train.merge(winseeds, on=['Season', 'WTeamID'])
 train = train.merge(lossseeds, on=['Season', 'LTeamID'])
 train['SeedDiff'] = train.WSeed - train.LSeed
+
+win_adj = adj_adv_stat_team_season.rename(columns={
+    'TeamID': 'WTeamID',
+    # 'AdjTempo': 'WAdjTempo',
+    # 'AdjORating': 'WAdjORating',
+    # 'AdjDRating': 'WAdjDRating',
+    'AdjNRating': 'WAdjNRating'
+})
+lose_adj = adj_adv_stat_team_season.rename(columns={
+    'TeamID': 'LTeamID',
+    # 'AdjTempo': 'LAdjTempo',
+    # 'AdjORating': 'LAdjORating',
+    # 'AdjDRating': 'LAdjDRating',
+    'AdjNRating': 'LAdjNRating'
+})
+
+train = train.merge(win_adj, on=['Season', 'WTeamID'])
+train = train.merge(lose_adj, on=['Season', 'LTeamID'])
+
 train = train.drop(['Season', 'WTeamID', 'LTeamID'], axis=1)
 train['Result'] = 1
 
 train_copy = train.copy()
 
-train_w_stat = ['WORating', 'WDRating', 'WSeed']
-train_l_stat = ['LORating', 'LDRating', 'LSeed']
+train_w_stat = ['WORating', 'WDRating', 'WSeed', 'WAdjNRating']
+train_l_stat = ['LORating', 'LDRating', 'LSeed', 'LAdjNRating']
 
 train_copy[train_w_stat + train_l_stat] = train_copy[train_l_stat + train_w_stat]
+train_copy['SeedDiff'] = -train_copy['SeedDiff']
 train_copy['Result'] = 0
 
 train = pd.concat([train, train_copy], ignore_index=True)
 
 # Print to show training data used
 print(train.head(n=5))
+print(train.tail())
 
 y_train = train['Result'].values
 X_train = train.drop('Result', axis=1)
@@ -79,9 +111,13 @@ prediction = prediction.merge(win_adv_stats, on=['Season', 'WTeamID'])
 prediction = prediction.merge(lossseeds, on=['Season', 'LTeamID'])
 prediction = prediction.merge(winseeds, on=['Season', 'WTeamID'])
 prediction['SeedDiff'] = prediction.WSeed - prediction.LSeed
+
+prediction = prediction.merge(lose_adj, on=['Season', 'LTeamID'])
+prediction = prediction.merge(win_adj, on=['Season', 'WTeamID'])
+
 prediction = prediction.drop(['Season', 'WTeamID', 'LTeamID'], axis=1)
 
 submission['Pred'] = clf.predict_proba(prediction)
 simulate_submit(submission)
 
-# submission.to_csv('logreg_seed_starter.csv', index=False)
+submission.to_csv('logreg_seed_starter.csv', index=False)
